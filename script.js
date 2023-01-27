5a7a2a83d6d998c8da21e56a92c328f295dfa74a
João Vitor Marques Braga <102833436+Joao-Vitor-Marques-Braga@users.noreply.github.com>
2023-01-27 09:29:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ const menu = [
         title: 'Hamburguer com fritas',
         category:'Almoço',
         price: 'R$:25',
-        img: './images/hamburguer-com-fritas.jpg',
+        img: './images/Hamburguer-com-fritas.jpg',
         desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
     },
 
